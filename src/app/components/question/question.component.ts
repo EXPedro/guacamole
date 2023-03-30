b66bf304d540e587dcf3d820d812b1aa1371e4f9
EXPedro <pedroelisio555@gmail.com>
2023-03-30 19:44:58 -0300

    create ng components variables

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.css']
 })
-export class QuestionComponent {
+export class QuestionComponent implements OnInit {
+
+  questions: any
+  questionSelected: any
+  questionIndex: number = 0
+  questionMaxIndex: number = 0
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
 
 }
--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.css']
 })
-export class ResultComponent {
+export class ResultComponent implements OnInit {
+
+  answers: string[] = []
+  answerSelected: string = ""
+  finished: boolean = false
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
 
 }
